21de4293d3b93004293f9f0ca886ac31649ac66b
LucasGaleano <l.a.galeano2@gmail.com>
2018-04-12 20:42:04 -0300

    agrego primera funcion de bloques: bloque1

--- blockChains.hs
+++ blockChains.hs
@@ -12,29 +12,42 @@ usuarioCon10Monedas = Usuario "Pepe" 10 10
 
 ejecutarTest = hspec $ do
 
-  it "Depositar 10 más. Debería quedar con 20 monedas." $ (billetera.deposita 10) usuarioCon10Monedas  `shouldBe` 20 
-  it "Extraer 3: Debería quedar con 7" $ (billetera.extraccion 3) usuarioCon10Monedas `shouldBe` 7
-  it "Extraer 15: Debería quedar con 0" $ (billetera .extraccion 15) usuarioCon10Monedas `shouldBe` 0
-  it "Un upgrade: Debería quedar con 12." $  (billetera.upgrade) usuarioCon10Monedas `shouldBe` 12
-  it "Cerrar la cuenta: 0." $ billetera (cierraCuenta usuarioCon10Monedas) `shouldBe` 0
-  it "Queda igual: 10." $ (billetera.quedaIgual) usuarioCon10Monedas `shouldBe` 10
-  it "Depositar 1000, y luego tener un upgrade: 1020." $ (billetera.upgrade.deposita 1000) usuarioCon10Monedas `shouldBe` 1020
-  it "Toco y me voy, deberia quedar con 0." $ (billetera.tocoMeVoy) usuarioCon10Monedas `shouldBe` 0
-  it "ahorrante errante, deberia quedar con 34." $ (billetera.ahorranteErrante) usuarioCon10Monedas `shouldBe` 34
+  describe "Pruebas sobre los eventos" $ do
 
+    it "Depositar 10 más. Debería quedar con 20 monedas." $ (billetera.deposita 10) usuarioCon10Monedas  `shouldBe` 20 
+    it "Extraer 3: Debería quedar con 7" $ (billetera.extraccion 3) usuarioCon10Monedas `shouldBe` 7
+    it "Extraer 15: Debería quedar con 0" $ (billetera .extraccion 15) usuarioCon10Monedas `shouldBe` 0
+    it "Un upgrade: Debería quedar con 12." $  (billetera.upgrade) usuarioCon10Monedas `shouldBe` 12
+    it "Cerrar la cuenta: 0." $ billetera (cierraCuenta usuarioCon10Monedas) `shouldBe` 0
+    it "Queda igual: 10." $ (billetera.quedaIgual) usuarioCon10Monedas `shouldBe` 10
+    it "Depositar 1000, y luego tener un upgrade: 1020." $ (billetera.upgrade.deposita 1000) usuarioCon10Monedas `shouldBe` 1020
+    it "Toco y me voy, deberia quedar con 0." $ (billetera.tocoMeVoy) usuarioCon10Monedas `shouldBe` 0
+    it "ahorrante errante, deberia quedar con 34." $ (billetera.ahorranteErrante) usuarioCon10Monedas `shouldBe` 34
 
-
+  describe "Pruebas sobre las transiciones" $ do
+    
+    it "Lucho toca y se va con billetera de 10 monedas, deberia quedar 0 monedas." $ (billetera.luchoTocaYSeVa lucho) usuarioCon10Monedas `shouldBe` 0
+    it "Lucho es ahorrante errante con billetera de 10 monedas, deberia quedar 34 monedas." $ (billetera.luchoEsUnAhorranteErrante lucho) usuarioCon10Monedas `shouldBe` 34
+    it "pepe le paga 7 monedas a lucho, deberia quedarle 3 monedas" $ (billetera.pepeLeDa7UnidadesALucho pepe) usuarioCon10Monedas `shouldBe` 3
+    it "lucho recibe 7 monedas de pepe, deberia quedarle 17 monedas" $ (billetera.pepeLeDa7UnidadesALucho lucho) usuarioCon10Monedas `shouldBe` 17
+    it "lucho cierra la cuenta aplicada a pepe, deberia quedar pepe sin cambios" $ luchoCierraLaCuenta pepe pepe `shouldBe` pepe
+    it "pepe le da 7 unidades a Lucho, deberia quedar lucho con 9 monedas" $ pepeLeDa7UnidadesALucho lucho lucho `shouldBe` nuevaBilletera 9 lucho
+    it "pepe le da 7 unidades a Lucho y despues pepe deposita 5 monedas, deberia quedar con 9 monedas" $ (pepeLeDa7UnidadesALucho pepe.pepeDeposita5Monedas pepe) pepe `shouldBe` nuevaBilletera 8 pepe
 
 -----------TIPOS DE DATOS-----------------
 
-type Transaccion = Usuario
-type Evento = Usuario
+type Evento = Usuario -> Usuario
+type Transaccion = Evento
+type Pago = Evento
+type Monedas = Float
+type Nombre = String
+type Nivel = Int 
 
 data Usuario = Usuario {
-  nombre :: String,
-  billetera :: Float,
-  nivel :: Int
-} deriving (Show) 
+  nombre :: Nombre,
+  billetera :: Monedas,
+  nivel :: Nivel
+} deriving (Show,Eq) 
 
 --------------USUARIOS-------------------
 
@@ -61,26 +74,46 @@ upgrade usuario
 
 ------------------TRANSACCIONES------------------
 
-luchoCierraLaCuenta :: Transaccion -> Evento
-luchoCierraLaCuenta unUsuario | nombre unUsuario == nombre lucho = cierraCuenta unUsuario
-                              | otherwise = quedaIgual unUsuario
+luchoCierraLaCuenta :: Usuario -> Transaccion
+luchoCierraLaCuenta unUsuario | compararUsuarios unUsuario lucho = cierraCuenta 
+                              | otherwise = quedaIgual
 
-pepeDeposita5Monedas :: Transaccion -> Evento
-pepeDeposita5Monedas unUsuario | nombre unUsuario == nombre pepe = deposita 5 unUsuario
-                               | otherwise = quedaIgual unUsuario 
+pepeDeposita5Monedas :: Usuario -> Transaccion
+pepeDeposita5Monedas unUsuario | compararUsuarios unUsuario pepe = deposita 5 
+                               | otherwise = quedaIgual  
 
-luchoTocaYSeVa :: Transaccion -> Evento
-luchoTocaYSeVa unUsuario unUsuario | nombre unUsuario == nombre lucho = tocoMeVoy unUsuario
-                                   | otherwise = quedaIgual unUsuario 
+luchoTocaYSeVa :: Usuario -> Transaccion
+luchoTocaYSeVa unUsuario | compararUsuarios unUsuario lucho = tocoMeVoy 
+                         | otherwise = quedaIgual  
 
-luchoEsUnAhorranteErrante :: Transaccion -> Evento
-luchoEsUnAhorranteErrante unUsuario | nombre unUsuario == nombre lucho = ahorranteErrante unUsuario
-                                    | otherwise = quedaIgual unUsuario 
-
-
+luchoEsUnAhorranteErrante :: Usuario -> Transaccion
+luchoEsUnAhorranteErrante unUsuario | compararUsuarios unUsuario lucho = ahorranteErrante 
+                                    | otherwise = quedaIgual  
 
 
 
+-----------------------PAGOS----------------------
+
+pepeLeDa7UnidadesALucho :: Usuario -> Pago
+pepeLeDa7UnidadesALucho unUsuario | compararUsuarios unUsuario pepe = extraccion 7 
+                                  | compararUsuarios unUsuario lucho = deposita 7
+                                  | otherwise = quedaIgual 
+
+
+-----------------------BLOQUES----------------------------------
+
+bloque1 unUsuario =  (luchoCierraLaCuenta unUsuario
+                      .pepeDeposita5Monedas unUsuario
+                      .pepeDeposita5Monedas unUsuario
+                      .pepeDeposita5Monedas unUsuario
+                      .luchoTocaYSeVa unUsuario
+                      .luchoEsUnAhorranteErrante unUsuario
+                      .pepeLeDa7UnidadesALucho unUsuario
+                      .luchoTocaYSeVa unUsuario) unUsuario
+ 
+-------------FUNCIONES AUXILIARES-----------------
+
+compararUsuarios usuario1 usuario2 = nombre usuario1 == nombre usuario2
 
 
 
